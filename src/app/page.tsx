dde947a4e95bbadfc8fbb096faed41f96928b903
Rizwana <159348120+rizwanaperveen@users.noreply.github.com>
2024-10-14 23:08:42 +0500

    Add files via upload
    build with next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import UnitConverterComponent from "@/components/ui/unit-converter";
+
+export default function Home() {
+  return (
+    <div>
+      <UnitConverterComponent />  
+    </div>
+  );
+}
